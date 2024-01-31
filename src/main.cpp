e0b4e123944c36c0fb7e920cbea972609148b7f3
Bruce Li <isanoob1897@outlook.com>
2024-01-30 19:07:14 -0800

    update

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+// all of the functions linked thogether
+#include "vk.h"
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <functional>
+
+int main() {
+    std::string sourcePath, destinationPath;
+}
--- src/mv.cpp
+++ src/mv.cpp
@@ -1,4 +1,20 @@
 #include "vk.h"
 #include <iostream>
-#include <cstdio>
+#include <cstdio> // remove function
 #include <sys/stat.h>
+// move file function
+
+bool mv(const std::string& sourcePath, const std::string& destinationPath) {
+    struct stat buffer;
+    if (stat(sourcePath.c_str(), &buffer) != 0) {
+        // cerr prints out like errors
+        std::cerr << "mv: No such file or directory" << std::endl;//newline
+        return false;
+    }
+    if (rename(sourcePath.c_str(), destinationPath.c_str()) != 0) {
+        std::cerr << "mv: Error moving file" << std::endl;
+        return false;
+    }
+    // if successful
+    return true;
+}
